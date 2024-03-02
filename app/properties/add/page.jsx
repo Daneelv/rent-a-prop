eee6e628b9f1aa24016698e76ce62bca82528de1
Daneel van Tonder <dnlvantonder@gmail.com>
2024-03-02 19:48:12 +0200

    WIP: Added Property Cards,  Added homepage, custom 404 and  spinner

---
+++ app/properties/add/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function PropertiesAddPage() {
+  return (
+    <div>
+      <h1 className="text-3xl">Properties Add Page</h1>
+      <Link href="/">Go Home</Link>
+    </div>
+  );
+}
+
+export default PropertiesAddPage;
