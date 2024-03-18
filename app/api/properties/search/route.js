feb72da25af0f4b0a8fa1d1a94b528381def1c31
Daneel van Tonder <dnlvantonder@gmail.com>
2024-03-18 08:02:13 +0200

    WIP:Property Search Component

---
+++ app/api/properties/search/route.js
@@ -0,0 +1,44 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+//GERT /api/properties/search
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(request.url);
+    const location = searchParams.get("location");
+    const propertyType = searchParams.get("propertyType");
+
+    const locationPattern = new RegExp(location, "i");
+
+    // Match Location pattern against database fields
+    let query = {
+      $or: [
+        { name: locationPattern },
+        { description: locationPattern },
+        { "location.street": locationPattern },
+        { "location.city": locationPattern },
+        { "location.state": locationPattern },
+        { "location.zipCode": locationPattern },
+      ],
+    };
+
+    // only check for property if its not 'All'
+    if (propertyType && propertyType !== "All") {
+      const typePattern = new RegExp(propertyType, "i");
+      query.type = typePattern;
+    }
+
+    const properties = await Property.find(query);
+
+    return new Response(JSON.stringify(properties), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ message: "Something Went Wrong" }), {
+      status: 500,
+    });
+  }
+};
